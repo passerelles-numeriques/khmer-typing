f55d37fe7d598f30b27180b38a9d3f2f729cc5a8
Laura <laura.hennequin@gmail.com>
2019-06-27 14:54:18 +0700

    Add nav bar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <nav>
+          <div class="top-nav">
+              <div class='nav-item'><router-link id='lblHome' v-bind:to="'/'">Home</router-link></div>
+          </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -11,5 +16,37 @@
 </script>
 
 <style>
-  /* CSS */
+  @font-face {
+    font-family: 'Noto Sans Khmer';
+    src: url('./assets/fonts/NotoSansKhmer-Regular.ttf') format('truetype');
+  }
+
+  .wrapper {
+    background-color: #353535;
+    font: "Noto Sans Khmer";
+  }
+
+  body {
+    margin: 0;
+    font-family: 'Noto Sans Khmer', Arial, Helvetica, sans-serif;
+  }
+
+  .top-nav {
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  .nav-item {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+
+  .nav-item:hover {
+    background-color: #ddd;
+    color: black;
+  }
 </style>
