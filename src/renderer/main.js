5922cfbaab41d375b2b83a96c3159442d0711539
Laura <laura.hennequin@gmail.com>
2019-07-01 16:15:45 +0700

    Grapheme-splitter plugin

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 
 import App from './App'
 import router from './router'
+import myPlugin from './grapheme-splitter-plugin.js'
+
+Vue.use(myPlugin)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  components: {
+    App
+  },
   router,
   template: '<App/>'
 }).$mount('#app')
