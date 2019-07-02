343fafeee6507a0dec8cac32d07ae2205af9988b
Laura <laura.hennequin@gmail.com>
2019-07-02 10:41:49 +0700

    Implement visual keyboard

---
+++ src/renderer/assets/hands.js
@@ -0,0 +1,71 @@
+module.exports = {
+  'SPACE': 'right-finger-1 left-finger-1',
+
+  'six': 'right-finger-2',
+  'Y': 'right-finger-2',
+  'H': 'right-finger-2',
+  'N': 'right-finger-2',
+
+  'seven': 'right-finger-2',
+  'U': 'right-finger-2',
+  'J': 'right-finger-2',
+  'M': 'right-finger-2',
+
+  'eight': 'right-finger-3',
+  'I': 'right-finger-3',
+  'K': 'right-finger-3',
+  'COMMA': 'right-finger-3',
+
+  'nine': 'right-finger-4',
+  'O': 'right-finger-4',
+  'L': 'right-finger-4',
+  'POINT': 'right-finger-4',
+
+  'zero': 'right-finger-5',
+  'P': 'right-finger-5',
+  'SEMICOLON': 'right-finger-5',
+  'ALT_GR': 'right-finger-1',
+  'DASH': 'right-finger-5',
+  'EQUAL': 'right-finger-5',
+  'BACKSPACE': 'right-finger-5',
+  'QUOTE': 'right-finger-5',
+  'LEFT_BRACKET': 'right-finger-5',
+  'RIGHT_BRACKET': 'right-finger-5',
+  'BACKSLASH': 'right-finger-5',
+  'ENTER': 'right-finger-5',
+  'SLASH': 'right-finger-5',
+  'RIGHT_SHIFT': 'right-finger-5',
+  'RIGHT_CTRL': 'right-finger-5',
+
+  'five': 'left-finger-2',
+  'T': 'left-finger-2',
+  'G': 'left-finger-2',
+  'B': 'left-finger-2',
+
+  'four': 'left-finger-2',
+  'R': 'left-finger-2',
+  'F': 'left-finger-2',
+  'V': 'left-finger-2',
+
+  'three': 'left-finger-3',
+  'E': 'left-finger-3',
+  'D': 'left-finger-3',
+  'C': 'left-finger-3',
+
+  'two': 'left-finger-4',
+  'W': 'left-finger-4',
+  'S': 'left-finger-4',
+  'X': 'left-finger-4',
+
+  'one': 'left-finger-5',
+  'Q': 'left-finger-5',
+  'A': 'left-finger-5',
+  'Z': 'left-finger-5',
+  'ALT': 'left-finger-1',
+
+  'ZWJ': 'left-finger-5',
+  'TAB': 'left-finger-5',
+  'CAPS_LOCK': 'left-finger-5',
+  'LEFT_SHIFT': 'left-finger-5',
+  'LEFT_CTRL': 'right-finger-5'
+}
