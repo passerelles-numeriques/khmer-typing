7d242fde590ae16e4fda065a91590ee43c4f0d52
Rith NHEL <parongnhel@gmail.com>
2019-07-08 10:05:30 +0700

    Translate into Khmer
    refs master

--- src/renderer/i18n-messages.js
+++ src/renderer/i18n-messages.js
@@ -82,44 +82,44 @@ export const messages = {
     "This project was born is PN's cambodian school, as a way to facilitate the learning of khmer typing for students. In order for it to benefit to as many people as possible, we then decided to make it open source and available on Windows and Linux stores.": "Ce projet est né dans l'école cambodgienne de PN, comme un moyen de faciliter l'apprentissage de la dactylographie khmère pour les étudiants. Afin qu'il profite au plus grand nombre, nous avons ensuite décidé de le rendre open source et disponible sur les stores Windows et Linux."
   },
   km: {
-    Home: 'ផ្ទះ',
+    Home: 'ទំព័រដើម',
     About: 'អំពី',
-    'Welcome!': 'Welcome!',
-    'Choose what you would like to do to practice Khmer typing today.': 'Choose what you would like to do to practice Khmer typing today.',
-    'Practise with Visual Keyboard': 'Practise with Visual Keyboard',
-    'Play Typefast game': 'Play Typefast game',
-    'View high scores': 'View high scores',
+    'Welcome!': 'សូមស្វាគមន៍!',
+    'Choose what you would like to do to practice Khmer typing today.': 'សូមជ្រើសរើសផ្នែកណាមួយដែលអ្នកចង់អនុវត្តន៍ ដើម្បីដំណើរការកម្មវិធី Khmer-Typing នៅក្នុងថ្ងៃនេះ។',
+    'Practise with Visual Keyboard': 'អនុវត្តជាមួយក្តារចុចជាក់ស្តែង',
+    'Play Typefast game': 'លេងហ្គេមកម្រិតលឿន',
+    'View high scores': 'បង្ហាញពិន្ទុអតិប្បរមា',
 
-    'Visual Keyboard': 'Visual Keyboard',
-    errors: 'errors',
+    'Visual Keyboard': 'ក្តារចុចជាក់ស្តែង',
+    errors: 'កំហុសឆ្គង',
 
-    'Typing game': 'ល្បែងនៃការរៀនសរសេរ',
-    'Type as many words as you can until time runs out!': 'វាយអោយបានច្រើនពាក្យតាមដែលអ្នកធ្វើបានរហូតដល់អស់នាទី',
+    'Typing game': 'ហ្គេមនៃការរៀនសរសេរ',
+    'Type as many words as you can until time runs out!': 'វាយអោយបានច្រើនពាក្យ តាមដែលអ្នកអាចធ្វើបាន រហូតដល់អស់នាទី',
     Start: 'ចាប់ផ្តើម',
     Score: 'ពិន្ទុ',
-    'Time left': 'ប្រើរយៈពេលអស់',
+    'Time left': 'រយះពេលនៅសល់',
     'What you are typing:': 'ពាក្យដែលអ្នកបានវាយ:',
 
     message: {
-      finishedPlaying: 'End of the game.',
-      scoreTypeFast: 'You wrote {score} words!',
-      scoreVisualKeyboard: 'Congrats! You finished in {minutes} minutes with {errors} errors.',
-      alertVisualKeyboard: 'Remember to switch your keyboard to Khmer language.'
+      finishedPlaying: 'ហ្គេមត្រូវបានបញ្ចប់',
+      scoreTypeFast: 'អ្នកបានសរសេរចំនួន {score} ពាក្យ!',
+      scoreVisualKeyboard: 'អបអរសាទរ! អ្នកបានបញ្ចប់ក្នុងរយះពេល {minutes} នាទី ជាមួយនឹងកំហុសឆ្គងចំនួន {errors}​ ។',
+      alertVisualKeyboard: 'អ្នកត្រូវផ្លាស់ប្តូរក្តារចុចទៅជាភាសាខ្មែរ។'
     },
-    Close: 'close',
-    'Alert!': 'Alert!',
+    Close: 'បិទ',
+    'Alert!': 'ជូនដំណឹង!',
 
     highScores: {
-      title: 'High scores',
-      description: 'Your greatest achievements.',
-      accuracy: 'In terms of accuracy - ',
-      speed: 'In terms of speed - ',
-      notPlayed: "You haven't played yet.",
-      visualKeyboard: 'You wrote {runes} runes in {time} minute(s), making {errors} error(s).',
-      typefast: 'You wrote {score} word(s) in one minute.'
+      title: 'ពិន្ទុខ្ពស់',
+      description: 'លទ្ធផលដ៏ល្អបំផុតរបស់អ្នក។',
+      accuracy: 'នៅក្នុងលក្ខខណ្ឌនៃភាពត្រឹមត្រូវ - ',
+      speed: 'នៅក្នុងលក្ខខណ្ឌនៃល្បឿន - ',
+      notPlayed: "អ្នកមិនទាន់លេងនៅឡើយទេ។",
+      visualKeyboard: 'អ្នកបានសរសេរចំនួន {runes} តួរអក្សរ នៅក្នុងរយះពេល {time} នាទី ដោយបានបង្កើតកំហុងឆ្គងចំនួន {errors} ។',
+      typefast: 'អ្នកសរសេរបានចំនួន {score} ពាក្យ នៅក្នុងរយះពេលមួយនាទី។'
     },
 
-    "Khmer-Typing was developped by a team working for the french NGO Passerelles Numériques (meaning 'Digital Bridges'). Our mission is to provide education, technical and professional training in the digital sector to young underprivileged people by leveraging their potential and willpower.": "Khmer-Typing was developped by a team working for the french NGO Passerelles Numériques (meaning 'Digital Bridges'). Our mission is to provide education, technical and professional training in the digital sector to young underprivileged people by leveraging their potential and willpower.",
-    "This project was born is PN's cambodian school, as a way to facilitate the learning of khmer typing for students. In order for it to benefit to as many people as possible, we then decided to make it open source and available on Windows and Linux stores.": "This project was born is PN's cambodian school, as a way to facilitate the learning of khmer typing for students. In order for it to benefit to as many people as possible, we then decided to make it open source and available on Windows and Linux stores."
+    "Khmer-Typing was developped by a team working for the french NGO Passerelles Numériques (meaning 'Digital Bridges'). Our mission is to provide education, technical and professional training in the digital sector to young underprivileged people by leveraging their potential and willpower.": "Khmer-Typing ត្រូវបានបង្កើតឡើងដោយក្រុមការងារសម្រាប់អង្គការក្រៅរដ្ឋាភិបាលមួយឈ្មោះថា ប៉ាស៊ឺរ៉ែលនុយមេរិច កម្ពុជា (មានន័យថា 'ស្ពានឌីជីថល') បេសកកម្មរបស់យើងគឺផ្តល់ការអប់រំបណ្តុះបណ្តាលបច្ចេកទេស និងវិជ្ជាជីវៈក្នុងផ្នែកឌីជីថលដល់មនុស្សក្រីក្រ ដែលមានវ័យក្មេងតាមរយៈសក្តានុពល និងឆន្ទៈរបស់ពួកគេ។",
+    "This project was born is PN's cambodian school, as a way to facilitate the learning of khmer typing for students. In order for it to benefit to as many people as possible, we then decided to make it open source and available on Windows and Linux stores.": "គម្រោងនេះត្រូវបានបង្កើតឡើងនៅក្នុងសាលាភីអ៊ិន កម្ពុជា ដើម្បីជាមធ្យោបាយសម្រួលដល់ការរៀនវាយសភាសាខ្មែរសម្រាប់និស្សិត។"
   }
 }
